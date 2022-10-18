7071f580466851b502f02e838647572d50e58127
Destray32 <kubaj38@gmail.com>
2022-10-18 18:38:20 +0200

    1sze cwiczenia i biblioteka

---
+++ cwiczenia-cpp.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+void WprowadzenieDanych(float& a, float& b);
+
+int main()
+{
+	float     a, b;
+	bool	czyKonty = true;
+	char	status;
+
+	while (czyKonty) {
+
+		WprowadzenieDanych(a, b);
+
+		if (a == 0)
+			b == 0 ? std::cout << "Nieskonczona ilosc M.Z\n" : std::cout << "Brak M.Z\n";
+		else
+			std::cout << "MZF to: " << -(b / a) << "\n";
+		
+		std::cout << "Czy chcesz kontynuować? Y/N\n";
+		std::cin >> status;
+		
+		status == 'Y' ? czyKonty = true : czyKonty = false;
+	}
+	return 0;
+}
+
+void WprowadzenieDanych(float& a, float& b)
+{
+	std::cout << "Podaj a\n";
+	std::cin >> a;
+	std::cout << "Podaj b\n";
+	std::cin >> b;
+
+	return;
+}
---
+++ przydatne-funkcje.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#ifndef PRZYDATNE_FUNKCJE_H
+#define PRZYDATNE_FUNKCJE_H
+
+
+
+
+#endif
